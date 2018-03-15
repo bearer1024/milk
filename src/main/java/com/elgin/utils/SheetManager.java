30f3b170ac32bf8236113c776435255988f04b2b
bearer1024 <bearer1024@gamil.com>
2018-03-15 12:47:16 +0800

    解决了读取证书有效期限类型的错误

--- src/main/java/com/elgin/utils/SheetManager.java
+++ src/main/java/com/elgin/utils/SheetManager.java
@@ -2,13 +2,14 @@ package com.elgin.utils;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Map;
 
 public class SheetManager {
     private final int FIRST_ROW_NO = 3;
@@ -20,6 +21,8 @@ public class SheetManager {
             OPCPackage  pkg = OPCPackage.open(remoteFile);
             XSSFWorkbook xssfWorkbook = new XSSFWorkbook(pkg);
             DataFormatter dataFormatter = new DataFormatter();
+            XSSFFormulaEvaluator xssfFormulaEvaluator = new XSSFFormulaEvaluator(xssfWorkbook);
+            String cellString = null;
             for(int i=FIRST_SHEET_NO;i<xssfWorkbook.getNumberOfSheets();i++){
                 Sheet sheet = xssfWorkbook.getSheetAt(i);
                 //System.out.println(xssfWorkbook.getSheetName(i));
@@ -29,24 +32,28 @@ public class SheetManager {
                         System.out.println("cell:"+cell);
                     }
                 }*/
-                for (int rowNum = FIRST_ROW_NO; rowNum < sheet.getLastRowNum(); rowNum++) {
+                for (int rowNum = FIRST_ROW_NO; rowNum <= sheet.getLastRowNum(); rowNum++) {
                     Row row = sheet.getRow(rowNum);
                     if (row == null) {
                         // This whole row is empty
                         // Handle it as needed
                         continue;
                     }
-
                     for (int colNum = FIRST_COLUMN_NO; colNum < row.getLastCellNum(); colNum++) {
                         //CellReference cellReference = new CellReference(rowNum,colNum);
                         Cell cell = row.getCell(colNum);
-                        if (cell == null) {
-                            // The spreadsheet is empty in this cell
-                        } else {
-                            //System.out.println(dataFormatter.formatCellValue(cell));
-                            /*System.out.println(cell.getStringCellValue());*/
-                            System.out.println(cell);
+                        switch (cell.getCellTypeEnum()){
+                            case NUMERIC:
+                                System.out.println("cell.getDateCellValue():"+cell.getDateCellValue());
+                            break;
+                            case STRING:
+                                cellString = dataFormatter.formatCellValue(cell);
+                                System.out.println("cellString is :"+cellString);
+                                break;
+                            default:
+                                continue;
                         }
+
                     }
                 }
 
@@ -58,6 +65,12 @@ public class SheetManager {
             e.printStackTrace();
         }
         return null;
+    }
+
+
+    public static void main(String[] args){
+        SheetManager sheetManager = new SheetManager();
+        sheetManager.readExcel(new File("/home/bearer/IdeaProjects/ssm_first/naifen/婴幼儿配方乳粉产品配方目录信息（2017年8月22日）.xlsx"));
     }
 
 }
